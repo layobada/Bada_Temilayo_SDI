4f2fe90768c5f15406ef578a66cda78e06341a4d
layobada <temilayobada@Temilayos-MacBook-Pro.local>
2015-05-14 18:27:50 -0400

    Rent per month
    Rent per month

--- Bada_Temilayo_Expressions_Assignment/js/Expressions.js
+++ Bada_Temilayo_Expressions_Assignment/js/Expressions.js
@@ -5,4 +5,18 @@ Section 02
 Expressions Assignment Week 2
  */
 
-alert("Testing to see if connected.");
+//alert("Testing to see if connected.");
+
+//Create a calculator for how much rent i pay per year
+
+//Ask the user for name
+var name = prompt ('Please type in your name.');
+
+//Send a welcome message to user with variable
+alert("welcome "+name+" ! ");
+
+//Ask user how much is the rent per month
+var rentPerMonth = prompt ("How much is rent per month?");
+
+//Apply console.log to check the result
+console.log(rentPerMonth);
