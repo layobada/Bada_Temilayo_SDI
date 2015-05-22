f2b5a5f2b80341aabdfef999f74fd2dfae06b4a0
layobada <tbada@fullsail.edu>
2015-05-21 21:09:24 -0400

    Array of animals in the zoo
    Array of animals in the zoo

--- Bada_Temilayo_Conditionals_Assignment/js/Conditionals.js
+++ Bada_Temilayo_Conditionals_Assignment/js/Conditionals.js
@@ -1,8 +1,28 @@
 /*
  Temilayo Bada
- 5/13/2015
+ 5/21/2015
  Section 02
- Conditionals Assignment Week 2
+ Conditionals Assignment Week 3
  */
 
 //alert("Testing to see if connected.");
+
+//Create a calculator that can sum up how many animals in a zoo
+
+//Create a zoo as an array
+var zoo = ["lion","tiger","monkey","elephant"];
+//print out the whole array
+console.log(zoo);
+
+
+//using index number to identify the array
+//index number 0,1,2,3
+
+console.log(zoo[0]);
+console.log(zoo[1]);
+console.log(zoo[2]);
+console.log(zoo[3]);
+
+//To check how many items are in the array using Dot Syntax
+console.log(zoo.length);
+
